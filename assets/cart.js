4e4021853efc1a1650df894a19dd4d42bd69c997
yakhyo04 <fenixrelaxation@gmail.com>
2023-08-27 14:20:31 +0500

    happened some changes

--- assets/cart.js
+++ assets/cart.js
@@ -1,54 +1,117 @@
 let form = document.getElementById('productsAllForm');
-
+let cartDrawwer = document.querySelector('.cart-drawer');
 let carts = []
-let variantsId = document.querySelector('.products__all--input:checked')
 
-form.addEventListener('submit', async(e) => {
-    let selectedVariants = [];
-    const totalQuantity = 3;
-    let variant_id = document.querySelectorAll('.products__all--input:checked')
-    let variant_count = document.querySelector('.productQuantity')
+form.addEventListener('submit', (e) => {
+var productCheckboxes  = document.querySelectorAll('.variant-id');
+var quantityInputs = document.querySelectorAll('.quantity-input');
+var productsToAdd = [];
+var totalQuantity = 0;
 
-    const cartItem = {
-        'items': [
-            {
-                id: Math.round(Math.random(10)*100)+1,
-                quantity: 1,
-                variants: selectedVariants
-            }
-        ]}
-    carts.push(cartItem)
-    const selectVariants = (variantId)=>{
-        if(selectedVariants.length<totalQuantity){
-            selectedVariants.push(variantId)
-        }else{
-            alert('total variants added')
-            // activateAddToCard()
-        }
+productCheckboxes.forEach(function(checkbox, index) {
+    if (checkbox.checked) {
+      var variantId = checkbox.value;
+      var quantity = parseInt(quantityInputs[index].value, 10);
+     
+      if (!isNaN(quantity) && quantity > 0) {
+        totalQuantity += quantity;
+        productsToAdd.push({ id: variantId, quantity: quantity });
+      }
     }
-    selectVariants(variant_id)
-    console.log(carts)
-    let formData = {
-        'items': [
-            {
-                'id': variant_id.value,
-                'quantity': variant_count.value
-            }
-        ]
-    }
+  });
 
-    await fetch(window.Shopify.routes.root + 'cart/add.js', {
+  function getSelectedQuantity() {
+    var radios = document.querySelectorAll(".quantity-radio");
+    for (var i = 0; i < radios.length; i++) {
+      if (radios[i].checked) {
+        return parseInt(radios[i].value, 10);
+      }
+    }
+    return null;
+  }
+
+  var selectedQuantity = getSelectedQuantity();
+
+  if (totalQuantity === selectedQuantity) {
+    var requestOptions = {
         method: 'POST',
         headers: {
-            dataType: 'json',
-            'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
-        body: cartItem
-    }).then(res => {
-        console.log(res)
-        return res.json()
-    })
-    .catch((error) => {
-        console.log('Error: ', error)
-    })
+        body: JSON.stringify({ items: productsToAdd }),
+        };
+        
+        fetch('/cart/add.js', requestOptions)
+        .then(response => response.json())
+        .then(data => {
+          console.log('Items added:', data);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+  } else {
+    alert("Please select 3, 6, or 12 products.");
+  }
 })
+
+// form.addEventListener('submit', async(e) => {
+//     // let selectedVariants = [];
+//     // const totalQuantity = 3;
+//     // let variant_id = document.querySelector('.products__all--input:checked')
+//     // let variant_count = document.querySelector('.productQuantity')
+
+//     // const cartItem = {
+//     //     'items': [
+//     //         {
+//     //             id: Math.round(Math.random(10)*100)+1,
+//     //             quantity: 1,
+//     //             variants: selectedVariants
+//     //         }
+//     //     ]}
+//     // carts.push(cartItem)
+//     // const selectVariants = (variantId)=>{
+//     //     if(selectedVariants.length<totalQuantity){
+//     //         selectedVariants.push(variantId)
+//     //     }else{
+//     //         alert('total variants added')
+//     //         // activateAddToCard()
+//     //     }
+//     // }
+//     // selectVariants(variant_id)
+//     // console.log(carts)
+//     var productItems = document.querySelectorAll(".product-item");
+//       var productsToAdd = [];
+
+//       productItems.forEach(function(productItem) {
+//         var variantId = productItem.querySelector(".variant-id").value;
+//         var quantityInput = productItem.querySelector(".quantity-input");
+//         var quantity = parseInt(quantityInput.value, 10);
+
+//         if (!isNaN(quantity) && quantity > 0) {
+//           productsToAdd.push({ id: variantId, quantity: quantity });
+//         }
+//       });
+//     // let formData = {
+//     //     'items': [
+//     //         {
+//     //             'id': variant_id.value,
+//     //             'quantity': variant_count.value
+//     //         }
+//     //     ]
+//     // }
+
+//     await fetch(window.Shopify.routes.root + 'cart/add.js', {
+//         method: 'POST',
+//         headers: {
+//             Accept: "application/json",
+//             'Content-Type': 'application/json'
+//         },
+//         body: productsToAdd
+//     }).then(res => {
+//         console.log(res)
+//         return res.json()
+//     })
+//     .catch((error) => {
+//         console.log('Error: ', error)
+//     })
+// })
